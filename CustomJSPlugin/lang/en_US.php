9c7db90f6e40c269ff7d63caa5352d3b1917da57
Bokor Pavol <bokor.pavol@gmail.com>
2020-07-01 11:40:43 +0200

    Create en_US.php

---
+++ CustomJSPlugin/lang/en_US.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * English Language File for Custom JS Plugin
+ *
+ * Date:		24 June 2020
+ * Revision:	0.1
+ * Version:		GetSimple 3.3
+ * Traductors: 	Pavol Bokor 
+ *
+ * @package GetSimple
+ * @subpackage Language
+ */
+
+$i18n = array (
+
+	"CUSTOMJS_TITLE"				=> "Custom JS",
+	"CUSTOMJS_DESC"					=> "Add custom Java&trade;Script to footer of theme",
+	"CUSTOMJS_JQ_URL"				=> "Insert jQuery URL",
+	"CUSTOMJS_CONTENT"				=> "Insert script",
+	"CUSTOMJS_URL_CONTENT_ERROR"			=> "jQuery URL is not valid."
+);
