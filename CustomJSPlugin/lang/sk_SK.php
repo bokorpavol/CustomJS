cda453a16324ceefe1574edfb1e986d1524a9280
Bokor Pavol <bokor.pavol@gmail.com>
2020-07-01 11:42:13 +0200

    Create sk_SK.php

---
+++ CustomJSPlugin/lang/sk_SK.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Slovak Language File for Custom JS Plugin
+ *
+ * Date:		24 June 2020
+ * Revision:	0.1
+ * Version:		GetSimple 3.3
+ * Traductors: 	Pavol Bokor 
+ *
+ * @package GetSimple
+ * @subpackage Language
+ */
+
+$i18n = array (
+
+	"CUSTOMJS_TITLE"				=> "Custom JS",
+	"CUSTOMJS_DESC"					=> "Pridá vlastnu Java&trade;Script do päty motívu",
+	"CUSTOMJS_JQ_URL"				=> "Vložte jQuery URL adresu ",
+	"CUSTOMJS_CONTENT"				=> "Vložte skript",
+	"CUSTOMJS_URL_CONTENT_ERROR"			=> "jQuery URL adresa nie je správna."
+);
