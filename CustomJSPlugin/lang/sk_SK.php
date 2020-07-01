22a5008bf78e94e241cee2a63de33d935ce0b98c
Bokor Pavol <bokor.pavol@gmail.com>
2020-07-01 14:27:43 +0200

    Update sk_SK.php

--- CustomJSPlugin/lang/sk_SK.php
+++ CustomJSPlugin/lang/sk_SK.php
@@ -17,5 +17,6 @@ $i18n = array (
 	"CUSTOMJS_DESC"					=> "Pridá vlastnu Java&trade;Script do päty motívu",
 	"CUSTOMJS_JQ_URL"				=> "Vložte jQuery URL adresu ",
 	"CUSTOMJS_CONTENT"				=> "Vložte skript",
-	"CUSTOMJS_URL_CONTENT_ERROR"			=> "jQuery URL adresa nie je správna."
+	"CUSTOMJS_URL_CONTENT_ERROR"	=> "jQuery URL adresa nie je správna.",
+	"CUSTOMJS_TOOLTIP"				=> "Vložte skript bez &lt;script&gt;&lt;/script&gt; značiek"
 );
